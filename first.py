9b611f4e9b955d9c9f088e44dab342460fd2cff8
Ekatche <71089627+Ekatche@users.noreply.github.com>
2021-02-01 09:32:38 +0100

    Update first.py

--- first.py
+++ first.py
@@ -115,7 +115,7 @@ st.plotly_chart(fig3)
 #Graph
 fig4 = px.scatter(df_C4, x='Confirmed', y='% of deaths', size='% of deaths', color='Country_Region',
                   title='Ranking of countries according to the % of deaths <br>' 
-                  '<div class="eq-c"> <div class="fraction"><span class="fup"><i>nb of death</span><span class="bar">/</span><span class="fdn"><i>nb of confirmed cases</span>')
+                  <math>\frac{'nb of deaths'}{'Confirmed cases}</math>')
 st.plotly_chart(fig4)
 #Graph
 fig5 = px.treemap(df_C3, path=['Country_Region'], values='Confirmed', title='World TreeMap - Confirmed cases')
