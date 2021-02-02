9fe810b26debde3f3d39c77b66a5c4dd7188c485
Ekatche <71089627+Ekatche@users.noreply.github.com>
2021-02-02 01:51:20 +0100

    Update first.py

--- first.py
+++ first.py
@@ -14,14 +14,13 @@ st.header('World Graphs')
 with zipfile.ZipFile('coronavirus.zip', 'r') as zip_ref:
     zip_ref.extractall('corona')
 
+
 @st.cache
 def Load_data(nrows):
-    df = pd.read_csv('corona/covid_19_data.csv', sep=',', index_col='SNo',nrows=nrows)
-    df.columns = ['Observation_date', 'Province_state', 'Country_Region', 'Last_Update', 'Confirmed', 'Deaths', 'Recovered']
+    df = pd.read_csv('corona/covid_19_data.csv', sep=',', index_col='SNo', nrows=nrows)
+    df.columns = ['Observation_date', 'Province_state', 'Country_Region', 'Last_Update', 'Confirmed', 'Deaths',
+                  'Recovered']
 
-    # mettre les dates au meme format
-    df['Last_Update'] = pd.to_datetime(df['Last_Update'])
-    df['Observation_date'] = pd.to_datetime(df['Observation_date'])
     # Correction des données des pays
     # 'Bahamas'-->'Bahamas, The'
     df.loc[df.Country_Region.str.contains("Bahamas"), 'Country_Region'] = 'Bahamas'
@@ -61,16 +60,75 @@ def Load_data(nrows):
     df.loc[df.Country_Region.str.contains("St. Martin"), 'Country_Region'] = 'Saint Martin (French part)'
     # Mainland China--> China
     df.loc[df.Country_Region.str.contains("Mainland China"), 'Country_Region'] = "China"
+    # replace na data of province_state column by unknown
+    df['Province_state'] = df['Province_state'].fillna('Unknown')
     return df
+
 
 # Analyse
 df = Load_data(156292)
 
-df1 = df.groupby(['Observation_date'])[['Confirmed', 'Recovered', 'Deaths']].sum().reset_index()
-df1['Observation_date'] = df1['Observation_date'].dt.strftime('%m/%Y')
-df1_bis = df1.groupby('Observation_date').sum()
-temp = df1.groupby('Observation_date').sum().reset_index()
-temp = temp.melt(id_vars="Observation_date", value_vars=['Deaths', 'Recovered', 'Confirmed'], var_name='Case',
+# Data processing
+dfose = df.sort_values(by=['Country_Region', 'Province_state', 'Observation_date'], ascending=True).reset_index().copy()
+# Modify datatype
+dfose['Confirmed'] = dfose['Confirmed'].astype(int)
+dfose['Deaths'] = dfose['Deaths'].astype(int)
+dfose['Recovered'] = dfose['Recovered'].astype(int)
+# create empty lists
+confirmed = [0] * len(dfose["Confirmed"])
+deaths = [0] * len(dfose["Confirmed"])
+recovered = [0] * len(dfose["Confirmed"])
+
+for i in range(len(dfose["Confirmed"])):
+    if i == 0:
+
+        confirmed[i] = dfose["Confirmed"][i]
+        deaths[i] = dfose["Deaths"][i]
+        recovered[i] = dfose["Recovered"][i]
+
+    elif (dfose['Province_state'][i] == dfose['Province_state'][i - 1]) and (
+            dfose['Country_Region'][i] != dfose['Country_Region'][i - 1]):
+        confirmed[i] = dfose["Confirmed"][i]
+        deaths[i] = dfose["Deaths"][i]
+        recovered[i] = dfose["Recovered"][i]
+
+    elif (dfose['Province_state'][i] != dfose['Province_state'][i - 1]) and (
+            dfose['Country_Region'][i] == dfose['Country_Region'][i - 1]):
+        confirmed[i] = dfose["Confirmed"][i]
+        deaths[i] = dfose["Deaths"][i]
+        recovered[i] = dfose["Recovered"][i]
+
+    elif (dfose['Province_state'][i] != dfose['Province_state'][i - 1]) and (
+            dfose['Country_Region'][i] != dfose['Country_Region'][i - 1]):
+        confirmed[i] = dfose["Confirmed"][i]
+        deaths[i] = dfose["Deaths"][i]
+        recovered[i] = dfose["Recovered"][i]
+    else:
+        confirmed[i] = abs(dfose["Confirmed"][i] - dfose["Confirmed"][i - 1])
+        recovered[i] = abs(dfose["Recovered"][i] - dfose["Recovered"][i - 1])
+        deaths[i] = abs(dfose["Deaths"][i] - dfose["Deaths"][i - 1])
+
+dfose['confirmed'] = confirmed
+dfose['deaths'] = deaths
+dfose['recovered'] = recovered
+
+dfose[dfose['confirmed'] < 0] = 0
+dfose[dfose['deaths'] < 0] = 0
+dfose[dfose['recovered'] < 0] = 0
+
+dfose = dfose.drop(columns=['Confirmed', 'Deaths', 'Recovered', 'SNo'])
+
+df0 = dfose.copy()
+df0['Observation_date'] = pd.to_datetime(df0['Observation_date'])
+df0['Observation_date'] = df0['Observation_date'].dt.strftime('%m/%Y')
+
+df1 = df0.groupby(['Observation_date'])[['confirmed', 'recovered', 'deaths']].sum().reset_index()
+df1.sort_values(by='Observation_date')
+
+st.write(df1)
+
+temp = df1
+temp = temp.melt(id_vars="Observation_date", value_vars=['deaths', 'recovered', 'confirmed'], var_name='Case',
                  value_name='Number of cases')
 fig = px.area(temp, x="Observation_date", y="Number of cases", color='Case', height=400,
               title='Global evolution of Covid-19 cases over time ')
@@ -79,53 +137,54 @@ fig = px.area(temp, x="Observation_date", y="Number of cases", color='Case', hei
 st.plotly_chart(fig)
 
 # Graph with widgets
-df10 = df.groupby(['Country_Region','Observation_date'])[['Confirmed', 'Recovered', 'Deaths']].sum().reset_index()
+
+df10 = dfose.groupby(['Country_Region', 'Observation_date'])[['confirmed', 'recovered', 'deaths']].sum().reset_index()
+df10['Observation_date'] = pd.to_datetime(df10['Observation_date'])
 df10['Observation_date'] = df10['Observation_date'].dt.strftime('%m/%Y')
 df1_Country = pd.DataFrame(df10.groupby(['Country_Region']).Observation_date.count())
-df1_Country=df1_Country[df1_Country['Observation_date']>50]
+df1_Country = df1_Country[df1_Country['Observation_date'] > 50]
 list_countries = list(df1_Country.index.unique())
 list_countries.sort()
 
 st.subheader('Evolution over the year per country')
 
-country=st.selectbox('Select a Country',list_countries)
+country = st.selectbox('Select a Country', list_countries)
 st.write(country)
-df10_bis = df10[df10.Country_Region == country ].groupby(['Observation_date'])[['Confirmed','Recovered','Deaths']].sum().reset_index()
+df10_bis = df10[df10.Country_Region == country].groupby(['Observation_date'])[
+    ['confirmed', 'recovered', 'deaths']].sum().reset_index()
 df_country = df10_bis.groupby('Observation_date').sum()
 st.area_chart(df_country)
 
 # World Analysis
 
-df_C = df.groupby(['Country_Region'])[['Confirmed', 'Deaths', 'Recovered']].sum().reset_index()
-df_C = df_C.sort_values(by="Deaths", ascending=False).head(50)
-df_C1 = df_C.sort_values(by="Recovered", ascending=False).head(50)
-df_C3 = df_C.sort_values(by="Confirmed", ascending=False).head(50)
-df_C['% of deaths'] = round(((df_C.Deaths / df_C.Confirmed) * 100), 2)
+df_C = dfose.groupby(['Country_Region'])[['confirmed','deaths','recovered']].sum().reset_index()
+df_C= df_C.sort_values(by="deaths", ascending= False).head(50)
+df_C1 = df_C.sort_values(by="recovered", ascending=False).head(50)
+df_C3 = df_C.sort_values(by="confirmed", ascending=False).head(50)
+df_C['% of deaths'] = round(((df_C.deaths / df_C.confirmed) * 100), 2)
 df_C4 = df_C.sort_values(by="% of deaths", ascending=False).head(50)
 
-#Graph
-fig2 = px.scatter(df_C, x='Confirmed', y='Deaths', size='Deaths', color='Country_Region',
+# Graph
+fig2 = px.scatter(df_C, x='confirmed', y='deaths', size='deaths', color='Country_Region',
                   title='Ranking of countries per number of deaths due to covid (TOP 50)')
 st.plotly_chart(fig2)
-#Graph
-fig3 = px.scatter(df_C1, x='Confirmed', y='Recovered', size='Recovered',
+# Graph
+fig3 = px.scatter(df_C1, x='confirmed', y='recovered', size='recovered',
                   color='Country_Region', title='Ranking of countries according to the number <br>'
                                                 'of recovery out of the confirmed cases (TOP 50)')
 st.plotly_chart(fig3)
-#Graph
-fig4 = px.scatter(df_C4, x='Confirmed', y='% of deaths', size='% of deaths', color='Country_Region',
-                  title='Ranking of countries according to the % of deaths <br>'
-                  '(Deaths/Confirmed)')
+# Graph
+fig4 = px.scatter(df_C4, x='confirmed', y='% of deaths', size='% of deaths', color='Country_Region',
+                  title='Ranking of countries according to the % of deaths')
 st.plotly_chart(fig4)
-#Graph
-fig5 = px.treemap(df_C3, path=['Country_Region'], values='Confirmed', title='World TreeMap - Confirmed cases')
+# Graph
+fig5 = px.treemap(df_C3, path=['Country_Region'], values='confirmed', title='World TreeMap - Confirmed cases')
 fig5.data[0].textinfo = 'label+text+value'
 st.plotly_chart(fig5)
 
-
-df_md = df.groupby('Country_Region')[['Confirmed', 'Deaths', 'Recovered']].sum().copy().reset_index()
+df_md = dfose.groupby('Country_Region')[['confirmed','deaths','recovered']].sum().copy().reset_index()
 list_countries = df_md['Country_Region'].unique().tolist()
-df_md = df_md.sort_values('Deaths', ascending=False)
+df_md = df_md.sort_values('deaths', ascending=False)
 d_country_code = {}  # To hold the country names and their ISO code 3
 
 for country in list_countries:
@@ -153,29 +212,29 @@ for k, v in d_country_code.items():
 
 fig = px.choropleth(data_frame=df_md,
                     locations="iso_alpha",
-                    color="Confirmed",  # value in column 'Confirmed' determines color
+                    color="confirmed",  # value in column 'Confirmed' determines color
                     hover_name="Country_Region",
                     color_continuous_scale=px.colors.sequential.Bluyl,  # color scale red, yellow green
                     title='COVID 19 World Map')
 st.plotly_chart(fig)
 
-fig = px.scatter_geo(df_md, locations="iso_alpha", color="Confirmed",
-                     hover_name="Country_Region", size="Confirmed",
+fig = px.scatter_geo(df_md, locations="iso_alpha", hover_name="Country_Region",
+                     color="confirmed", size="confirmed",
                      projection="natural earth")
 
 st.plotly_chart(fig)
 
-#Header
+# Header
 
 st.subheader('Focus USA')
 
-df_US = df[df['Country_Region'] == 'US'].copy()
+df_US=dfose[dfose['Country_Region']=='US'].copy()
 
-df_US_A = df_US.groupby(['Province_state'])[['Confirmed', 'Deaths', 'Recovered']].sum()
+df_USA= df_US.groupby(['Province_state'])[['confirmed','deaths','recovered']].sum().reset_index()
 
-df_US_A = df_US_A.sort_values('Confirmed', ascending=False).reset_index()
+df_USA=df_USA.sort_values('confirmed', ascending=False).reset_index()
 
-df_US_A = df_US_A.drop(197, axis=0)
+df_USA=df_USA.drop(196,axis=0)
 
 # declare an empty list to store
 # latitude and longitude values
@@ -183,6 +242,7 @@ longitude = []
 latitude = []
 
 geolocator = Nominatim(user_agent="geoapiExercises")
+
 
 @st.cache
 def findGeocode(Province_state):
@@ -199,10 +259,11 @@ def findGeocode(Province_state):
         return findGeocode(Province_state)
     # each value from city column
 
+
 # will be fetched and sent to
 # function find_geocode
 
-for i in (df_US_A["Province_state"]):
+for i in (df_USA["Province_state"]):
 
     if findGeocode(i) != None:
 
@@ -222,48 +283,47 @@ for i in (df_US_A["Province_state"]):
         longitude.append(np.nan)
 
     # of city column
-df_US_A['Longitude'] = longitude
-df_US_A['Latitude'] = latitude
+df_USA['Longitude'] = longitude
+df_USA['Latitude'] = latitude
 
-df_US_A['text'] = df_US_A['Province_state'] + '<br> Number of confirmed cases ' + (df_US_A['Confirmed']).astype(str)
+assert isinstance(df_USA, object)
+df_USA['text'] = df_USA['Province_state'] + '<br> Number of confirmed cases ' + (df_USA['confirmed']/1e6).astype(str) + ' million'
 
-affichage = [(40, 105), (16, 39), (5, 15), (0, 4)]
-limite = [(50, 1), (21, 50), (11, 20), (0, 10)]
-seuil = [(0, 5), (6, 20), (21, 50), (51, 198)]
-scale = 50000
+limits = [(0,13),(14,33),(34,49)]
+affichage = [(2.6e5,1e6),(1e5,2.5e5),(1e4,9e4)]
+colors = ["royalblue","crimson","lightseagreen","lightgrey"]
+scale = 400
 
-fig6 = go.Figure()
-for i in range(len(seuil)):
-    # lim = limits[i]
-    aff = affichage[i]
-    lim = seuil[i]
-    df_sub = df_US_A[lim[0]:lim[1]]
-    fig6.add_trace(go.Scattergeo(
-        locationmode='USA-states',
-        lon=df_sub['Longitude'],
-        lat=df_sub['Latitude'],
-        text=df_sub['text'],
-        marker=dict(
-            autocolorscale=True,
-            line_color='rgb(40,10,40,20)',
-            size=df_sub['Confirmed'] / scale,
-            line_width=0.5,
-            sizemode='area'), name='{} - {}'.format(aff[0], aff[1])))
+fig = go.Figure()
+for i in range(len(limits)):
+    lim = limits[i]
+    aff=affichage[i]
+    df_sub = df_USA[lim[0]:lim[1]]
+    fig.add_trace(go.Scattergeo(
+            locationmode = 'USA-states',
+            lon = df_sub['Longitude'],
+            lat = df_sub['Latitude'],
+            text = df_sub['text'],
+            marker = dict(
+                autocolorscale=True,
+                color=colors[i],
+                size = df_sub['confirmed']/scale,
+                line_width=0.5,
+                sizemode = 'area'),name = '{} - {}'.format(aff[0],aff[1])))
 
-fig6.update_layout(
-    title_text='2020 - Covid confirmed cases in the US (legend in million) <br>(Click legend to toggle traces)',
-    showlegend=True,
-    geo=dict(
-        scope='usa', landcolor='rgb(217,217,210)'))
+fig.update_layout(
+        title_text = '2020 Covid confirmed cases in the US <br>(Click legend to toggle traces)',
+        showlegend = True,
+        geo = dict(
+            scope = 'usa',landcolor = 'rgb(217,217,210)'))
 
-st.plotly_chart(fig6)
+st.plotly_chart(fig)
 
 st.header('Animated Graphs')
 
-df_md_gbd_1 = df.copy()
+df_md_gbd_1=dfose.copy()
 
 df_md_gbd_1['observation_date'] = pd.to_datetime(df_md_gbd_1['Observation_date'])
-
 df_md_gbd_1['observation_date'] = df_md_gbd_1['observation_date'].dt.strftime('%m/%Y')
 
 df_md_gbd_1 = df_md_gbd_1.groupby(['Country_Region', 'observation_date']).sum().groupby(level=0).cumsum().sort_values(
@@ -273,14 +333,13 @@ df_md_gbd_1['iso_alpha'] = ''
 for k, v in d_country_code.items():
     df_md_gbd_1.loc[(df_md_gbd_1.Country_Region == k), 'iso_alpha'] = v
 
-fig = px.scatter_geo(df_md_gbd_1, locations="iso_alpha", color="Confirmed",
-                     hover_name="Country_Region", size="Confirmed",
-                     projection="natural earth", animation_frame="observation_date",
-                     title='COVID 19 World Map - Evolution of confirmed cases per Months')
+fig = px.scatter_geo(df_md_gbd_1, locations="iso_alpha", color="confirmed",
+                     hover_name="Country_Region",size="confirmed",
+                     projection="natural earth",animation_frame="observation_date")
 
 st.plotly_chart(fig)
 
 st.header('COVID in 2020')
-video_file = open('BarChartRace2.mp4', 'rb')
+video_file = open('BarChartRace.mp4', 'rb')
 video_bytes = video_file.read()
 st.video(video_bytes)
