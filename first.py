017978710c10e535357511570ff5c0cf63a44def
Ekatche <71089627+Ekatche@users.noreply.github.com>
2022-03-25 14:23:16 +0100

    Update first.py
    modified geolocator

--- first.py
+++ first.py
@@ -244,7 +244,8 @@ df_USA=df_USA.drop(196,axis=0)
 longitude = []
 latitude = []
 
-geolocator = Nominatim(user_agent="geoapiExercises")
+# geolocator = Nominatim(user_agent="geoapiExercises")
+geolocator = Nominatim()
 
 
 @st.cache
