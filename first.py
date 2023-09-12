8c23aaa41fad687ec694688e4d831c8992c5fe83
Eliel katche <71089627+Ekatche@users.noreply.github.com>
2023-09-12 12:38:16 +0200

    Update first.py

--- first.py
+++ first.py
@@ -331,16 +331,14 @@ st.header('Animated Graphs')
 df_md_gbd_1=dfose.copy()
 
 df_md_gbd_1['observation_date'] = pd.to_datetime(df_md_gbd_1['Observation_date'])
-st.dataframe(df_md_gbd_1)
+# st.dataframe(df_md_gbd_1)
 
-
-df_md_gbd_2 = df_md_gbd_1.groupby(['Country_Region', 'observation_date']).sum().groupby(level=0)[['confirmed','deaths','recovered']].cumsum().sort_values(
+df_md_gbd_1 = df_md_gbd_1.groupby(['Country_Region', 'observation_date']).sum().groupby(level=0)[['confirmed','deaths','recovered']].cumsum().sort_values(
     by='observation_date').reset_index()
 
 # Group by 'Country_Region' and 'observation_date' and sum the numeric columns
-df_md_gbd_1 = df_md_gbd_1.groupby(['Country_Region', df_md_gbd_1['observation_date'].dt.strftime('%m/%Y')])[['confirmed','deaths','recovered']].sum().reset_index()
+# df_md_gbd_1 = df_md_gbd_1.groupby(['Country_Region', df_md_gbd_1['observation_date'].dt.strftime('%m/%Y')])[['confirmed','deaths','recovered']].sum().reset_index()
 
-st.dataframe(df_md_gbd_2)
 
 df_md_gbd_1['iso_alpha'] = ''
 for k, v in d_country_code.items():
